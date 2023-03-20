01811997b15b82efcb72db940550c9a5ff0eab3b
Kuba Kłeczek <jakub.kleczek@icloud.com>
2023-03-20 10:23:37 +0100

    Manipulating CSS Styles

--- script.js
+++ script.js
@@ -19,10 +19,19 @@ document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+    // when there is no input
     if (!guess) {
         document.querySelector('.message').textContent = '⛔️ No number!';
+
+        // when player wins
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        // when guess is too high
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = '📈 Too high!';
@@ -33,6 +42,8 @@ document.querySelector('.check').addEventListener('click', function () {
                 '💥 You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
+
+        // when guess is too low
     } else if (guess < secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = '📉 Too low!';
